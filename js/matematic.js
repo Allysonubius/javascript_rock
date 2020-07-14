29f54da4b99c5837833061f462f60ddd5f4d062e
Allysonubius <allysomoliveira7@gmail.com>
2020-07-14 12:45:27 -0300

    Estudo sobre operações matematicas realizado.

---
+++ js/matematic.js
@@ -0,0 +1,14 @@
+var x = 10,
+    y = 5;
+
+var sum = x + y;
+var subtraction = x - y;
+var multiply = x * y;
+var division = x / y;
+var modulo = x % y;
+
+console.log("Soma: " + sum);
+console.log("Subtração: " + subtraction);
+console.log("Multiplicação: " + multiply);
+console.log("Divisão: " + division);
+console.log("Resto: " + modulo);
