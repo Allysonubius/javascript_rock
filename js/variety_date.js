572c713d2a978c2aa84570a56de3175a535fec0b
Allysonubius <allysomoliveira7@gmail.com>
2020-07-14 12:34:07 -0300

    Estudo sobre Variaveis e dados com JavasScript.

---
+++ js/variety_date.js
@@ -0,0 +1,14 @@
+//JavaScrip Variaveis e Dados
+var nome = "Allyson ";
+var idade = 22;
+var peso = 80.5;
+var humano = true;
+var alunos = ['Diego', 'Gabriel', 'Lucas'];
+var alunos = {
+    nome: alunos[1],
+    idade: 23,
+    peso: 80.5,
+    humano: true
+};
+//console.log(alunos[1]);
+console.log(alunos.nome);
